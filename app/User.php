40b408d5276e3b4d03022f950032be9e0a9e571f
Dave Mills <d.e.mills@stats4sd.org>
2020-09-08 00:49:44 +0100

    adding user management

--- app/User.php
+++ app/User.php
@@ -4,12 +4,13 @@ namespace App;
 
 use App\Models\Alert;
 use Illuminate\Notifications\Notifiable;
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use CrudTrait, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -3,3 +3,4 @@
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('alert') }}'><i class='nav-icon la la-question'></i> Alerts</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('activity') }}'><i class='nav-icon la la-question'></i> Activities</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('recipient') }}'><i class='nav-icon la la-question'></i> Recipients</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-user'></i> <span>Users</span></a></li>
